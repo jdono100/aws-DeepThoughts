405176cbe08be312d0e30751038ff52142d20cc9
Jared Donovan <jdono100@gmail.com>
2021-10-02 15:20:21 -0500

    displays images

--- server/utils/params-config.js
+++ server/utils/params-config.js
@@ -6,7 +6,8 @@ const params = fileName => {
   const imageParams = {
     Bucket: 'user-images-38cfa409-826c-4245-8a0c-02e30fc02b56',
     Key: `${uuidv4()}.${fileType}`,
-    Body: fileName.buffer
+    Body: fileName.buffer, 
+    ACL: 'public-read' // allow read access to this file
   };
   return imageParams;
 };
